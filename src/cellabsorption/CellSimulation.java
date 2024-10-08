27f7f4abca3128a2a61310c8845609ec185afa98
snowpiperr <pmay@macalester.edu>
2024-10-07 18:55:40 -0700

    it does something else now? involves a list
    i was told not to write sassy commit messages :(

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -5,6 +5,8 @@ import edu.macalester.graphics.Ellipse;
 import edu.macalester.graphics.Point;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @SuppressWarnings("SameParameterValue")
@@ -12,8 +14,7 @@ public class CellSimulation {
     
     private CanvasWindow canvas;
     private Random rand = new Random();
-    private Cell cell;
-    
+    private List<Cell> cells;
     
 
     public static void main(String[] args) {
@@ -23,28 +24,37 @@ public class CellSimulation {
     public CellSimulation() {
         canvas = new CanvasWindow("Cell Absorption", 800, 800);
         double size = rand.nextInt(5) + 2;
+        cells = new ArrayList<Cell>();
         populateCells();
 
-        cell = new Cell(
-            rand.nextDouble() * (canvas.getWidth() - size),
-            rand.nextDouble() * (canvas.getWidth() - size),
-            size,
-            Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
-        cell.addToCanvas(canvas);
+
        
 
         //noinspection InfiniteLoopStatement
         while (true) {
-            Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
-            cell.moveAround(canvasCenter);
-            cell.grow(0.02);
-
+            for(Cell cell: cells) {
+                Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
+                cell.moveAround(canvasCenter);
+                cell.grow(0.02);
+                canvas.pause(10);
+            }
             canvas.draw();
-            canvas.pause(10);
         }
     }
 
     private void populateCells() {
+        
+        
+        for(int i = 0 ; i<200; i++){
+            double size = rand.nextInt(5) + 2;
+            Cell cell = new Cell(
+            rand.nextDouble() * (canvas.getWidth() - size),
+            rand.nextDouble() * (canvas.getWidth() - size),
+            size,
+            Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
+            cell.addToCanvas(canvas); 
+            cells.add(cell);      
+        }
         
     }
 
